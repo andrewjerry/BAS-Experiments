ce227b8b03e0ab0e95097f2402b4c6a1372ac7d9
andrewjerry <andrewmichaeljerry@gmail.com>
2023-03-10 02:01:11 -0600

    Update caldera_agent_bootstrap.sh

--- AWS/Bootstrap_Scripts/caldera_agent_bootstrap.sh
+++ AWS/Bootstrap_Scripts/caldera_agent_bootstrap.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-curl "https://github.com/andrewjerry/BAS-Experiments/blob/main/AWS/Bootstrap_Scripts/caldera_agent_install.sh" > bootstrap.sh
-chmod 755 bootstrap.sh
-./bootstrap.sh
-rm bootstrap.sh
+curl "https://github.com/andrewjerry/BAS-Experiments/blob/main/AWS/Bootstrap_Scripts/caldera_agent_install.sh" | bash
